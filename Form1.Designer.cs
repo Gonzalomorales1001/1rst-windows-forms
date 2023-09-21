e6e2a9a6e7ba5ff3351e56868ad10ddf29200623
Gonzalomorales1001 <gonzalomorales1001@gmail.com>
2023-09-21 18:10:51 -0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿
+namespace Laboratorio_Parcial_2
+{
+    partial class Parcial2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Parcial2));
+            this.Inputs = new System.Windows.Forms.Panel();
+            this.calc = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.percent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ingresoPorcentajeEscenario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tribuna = new System.Windows.Forms.Label();
+            this.stadiumCapacity = new System.Windows.Forms.Label();
+            this.ingresoPrecioTicketVIP = new System.Windows.Forms.TextBox();
+            this.ingresoPrecioTicket = new System.Windows.Forms.TextBox();
+            this.ingresoCapacidadTribuna = new System.Windows.Forms.TextBox();
+            this.ingresoCapacidadEstadio = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.totalTickets = new System.Windows.Forms.Label();
+            this.muestraEntradas = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.IngresosTotales = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.IngresosTicketVIP = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.IngresosTicket = new System.Windows.Forms.Label();
+            this.Inputs.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Inputs
+            // 
+            this.Inputs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Inputs.Controls.Add(this.calc);
+            this.Inputs.Controls.Add(this.label7);
+            this.Inputs.Controls.Add(this.label6);
+            this.Inputs.Controls.Add(this.label4);
+            this.Inputs.Controls.Add(this.percent);
+            this.Inputs.Controls.Add(this.label1);
+            this.Inputs.Controls.Add(this.ingresoPorcentajeEscenario);
+            this.Inputs.Controls.Add(this.label3);
+            this.Inputs.Controls.Add(this.label2);
+            this.Inputs.Controls.Add(this.tribuna);
+            this.Inputs.Controls.Add(this.stadiumCapacity);
+            this.Inputs.Controls.Add(this.ingresoPrecioTicketVIP);
+            this.Inputs.Controls.Add(this.ingresoPrecioTicket);
+            this.Inputs.Controls.Add(this.ingresoCapacidadTribuna);
+            this.Inputs.Controls.Add(this.ingresoCapacidadEstadio);
+            this.Inputs.Controls.Add(this.Title);
+            this.Inputs.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            resources.ApplyResources(this.Inputs, "Inputs");
+            this.Inputs.ForeColor = System.Drawing.SystemColors.Control;
+            this.Inputs.Name = "Inputs";
+            this.Inputs.UseWaitCursor = true;
+            // 
+            // calc
+            // 
+            this.calc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.calc.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.calc.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.calc, "calc");
+            this.calc.Name = "calc";
+            this.calc.UseVisualStyleBackColor = false;
+            this.calc.UseWaitCursor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label7.Name = "label7";
+            this.label7.UseWaitCursor = true;
+            this.label7.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Name = "label6";
+            this.label6.UseWaitCursor = true;
+            this.label6.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Name = "label4";
+            this.label4.UseWaitCursor = true;
+            // 
+            // percent
+            // 
+            resources.ApplyResources(this.percent, "percent");
+            this.percent.BackColor = System.Drawing.Color.Transparent;
+            this.percent.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.percent.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.percent.Name = "percent";
+            this.percent.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Name = "label1";
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // ingresoPorcentajeEscenario
+            // 
+            this.ingresoPorcentajeEscenario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ingresoPorcentajeEscenario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.ingresoPorcentajeEscenario, "ingresoPorcentajeEscenario");
+            this.ingresoPorcentajeEscenario.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ingresoPorcentajeEscenario.Name = "ingresoPorcentajeEscenario";
+            this.ingresoPorcentajeEscenario.UseWaitCursor = true;
+            this.ingresoPorcentajeEscenario.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Name = "label3";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Name = "label2";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tribuna
+            // 
+            resources.ApplyResources(this.tribuna, "tribuna");
+            this.tribuna.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.tribuna.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.tribuna.Name = "tribuna";
+            this.tribuna.UseWaitCursor = true;
+            // 
+            // stadiumCapacity
+            // 
+            resources.ApplyResources(this.stadiumCapacity, "stadiumCapacity");
+            this.stadiumCapacity.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.stadiumCapacity.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.stadiumCapacity.Name = "stadiumCapacity";
+            this.stadiumCapacity.UseWaitCursor = true;
+            // 
+            // ingresoPrecioTicketVIP
+            // 
+            this.ingresoPrecioTicketVIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ingresoPrecioTicketVIP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.ingresoPrecioTicketVIP, "ingresoPrecioTicketVIP");
+            this.ingresoPrecioTicketVIP.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ingresoPrecioTicketVIP.Name = "ingresoPrecioTicketVIP";
+            this.ingresoPrecioTicketVIP.UseWaitCursor = true;
+            // 
+            // ingresoPrecioTicket
+            // 
+            this.ingresoPrecioTicket.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ingresoPrecioTicket.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.ingresoPrecioTicket, "ingresoPrecioTicket");
+            this.ingresoPrecioTicket.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ingresoPrecioTicket.Name = "ingresoPrecioTicket";
+            this.ingresoPrecioTicket.UseWaitCursor = true;
+            // 
+            // ingresoCapacidadTribuna
+            // 
+            this.ingresoCapacidadTribuna.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ingresoCapacidadTribuna.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.ingresoCapacidadTribuna, "ingresoCapacidadTribuna");
+            this.ingresoCapacidadTribuna.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ingresoCapacidadTribuna.Name = "ingresoCapacidadTribuna";
+            this.ingresoCapacidadTribuna.UseWaitCursor = true;
+            // 
+            // ingresoCapacidadEstadio
+            // 
+            this.ingresoCapacidadEstadio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ingresoCapacidadEstadio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.ingresoCapacidadEstadio, "ingresoCapacidadEstadio");
+            this.ingresoCapacidadEstadio.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ingresoCapacidadEstadio.Name = "ingresoCapacidadEstadio";
+            this.ingresoCapacidadEstadio.UseWaitCursor = true;
+            this.ingresoCapacidadEstadio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Title
+            // 
+            resources.ApplyResources(this.Title, "Title");
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Title.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Title.Name = "Title";
+            this.Title.UseWaitCursor = true;
+            this.Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // totalTickets
+            // 
+            resources.ApplyResources(this.totalTickets, "totalTickets");
+            this.totalTickets.BackColor = System.Drawing.Color.Transparent;
+            this.totalTickets.Name = "totalTickets";
+            this.totalTickets.UseWaitCursor = true;
+            // 
+            // muestraEntradas
+            // 
+            resources.ApplyResources(this.muestraEntradas, "muestraEntradas");
+            this.muestraEntradas.Name = "muestraEntradas";
+            this.muestraEntradas.UseWaitCursor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.totalTickets);
+            this.groupBox1.Controls.Add(this.muestraEntradas);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.UseWaitCursor = true;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.IngresosTotales);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.IngresosTicketVIP);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.IngresosTicket);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            this.groupBox2.UseWaitCursor = true;
+            // 
+            // IngresosTotales
+            // 
+            resources.ApplyResources(this.IngresosTotales, "IngresosTotales");
+            this.IngresosTotales.Name = "IngresosTotales";
+            this.IngresosTotales.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.label8.UseWaitCursor = true;
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            this.label9.UseWaitCursor = true;
+            // 
+            // IngresosTicketVIP
+            // 
+            resources.ApplyResources(this.IngresosTicketVIP, "IngresosTicketVIP");
+            this.IngresosTicketVIP.Name = "IngresosTicketVIP";
+            this.IngresosTicketVIP.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            this.label5.UseWaitCursor = true;
+            this.label5.Click += new System.EventHandler(this.label5_Click_1);
+            // 
+            // IngresosTicket
+            // 
+            resources.ApplyResources(this.IngresosTicket, "IngresosTicket");
+            this.IngresosTicket.Name = "IngresosTicket";
+            this.IngresosTicket.UseWaitCursor = true;
+            // 
+            // Parcial2
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = global::Laboratorio_Parcial_2.Properties.Resources.subtle_prism;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Inputs);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MaximizeBox = false;
+            this.Name = "Parcial2";
+            this.ShowIcon = false;
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Parcial2_Load);
+            this.Inputs.ResumeLayout(false);
+            this.Inputs.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Inputs;
+        private System.Windows.Forms.TextBox ingresoCapacidadEstadio;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label stadiumCapacity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label tribuna;
+        private System.Windows.Forms.TextBox ingresoPrecioTicketVIP;
+        private System.Windows.Forms.TextBox ingresoPrecioTicket;
+        private System.Windows.Forms.TextBox ingresoCapacidadTribuna;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ingresoPorcentajeEscenario;
+        private System.Windows.Forms.Label percent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label totalTickets;
+        private System.Windows.Forms.Label muestraEntradas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label IngresosTicket;
+        private System.Windows.Forms.Label IngresosTotales;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label IngresosTicketVIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_Parcial_2
+{
+    public partial class Parcial2 : Form
+    {
+        public Parcial2()
+        {
+            InitializeComponent();
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Parcial2_Load(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void calc_Click(object sender, EventArgs e)
+        {
+            mostrarValores();
+        }
+
+        public void mostrarValores()
+        {
+            //creo una instancia de mi clase evento
+            Evento recital = new Evento();
+            //valido que los campos sean correctos (que no esten vacíos y que sean números)
+            //para eso, utilizo una funcion que retorna un valor booleano para asignarle a mi variable bandera
+            bool textboxsValidosBandera = validarTextBoxs();
+            //si son validos, ejecuto el then del if, y si no, muestro un mensaje con el metodo show
+            if (textboxsValidosBandera)
+            {
+                recital.estadioM2 = Convert.ToDouble(ingresoCapacidadEstadio.Text);
+                recital.capacidadTribuna = Convert.ToInt32(ingresoCapacidadTribuna.Text);
+                recital.escenarioPorcentaje = Convert.ToDouble(ingresoPorcentajeEscenario.Text);
+                recital.ticketPrecio = Convert.ToDouble(ingresoPrecioTicket.Text);
+                recital.ticketPrecioVIP = Convert.ToDouble(ingresoPrecioTicketVIP.Text);
+                //el retorno del metodo es la cantidad de entradas
+                muestraEntradas.Text = Convert.ToString(recital.calcularEntradas());
+                //el retorno del metodo es un vector que tiene los ingresos si se vendieran todas las entradas
+                double[] ingresos = recital.calcularIngresos(recital.calcularEntradas());
+                IngresosTicket.Text = "$" + Convert.ToString(ingresos[0]);
+                IngresosTicketVIP.Text = "$" + Convert.ToString(ingresos[1]);
+                IngresosTotales.Text = "$" + Convert.ToString(ingresos[2]);
+            }
+            else
+            {
+                //el mensaje que se muestra si es que los campos no son validos
+                MessageBox.Show("Los datos ingresados no son válidos","Error al calcular",MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+        public bool validarTextBoxs()
+        {
+            bool bandera=true;
+            //creo un vector que tiene como valor un objeto el cual es instancia de la clase TextBox
+            TextBox[] misTextBoxs = new TextBox[5];
+            //guardo manualmente mis textboxs en este vector
+            misTextBoxs[0] = ingresoCapacidadEstadio;
+            misTextBoxs[1] = ingresoCapacidadTribuna;
+            misTextBoxs[2] = ingresoPorcentajeEscenario;
+            misTextBoxs[3] = ingresoPrecioTicket;
+            misTextBoxs[4] = ingresoPrecioTicketVIP;
+            for(int i = 0; i < misTextBoxs.Length; i++)
+            {
+                //verifico si en cada posicion la longitud de su propiedad .Text es 0 (pa saber si ta vacio) o NO es un double
+                if (misTextBoxs[i].Text.Length == 0 || !double.TryParse(misTextBoxs[i].Text, out double resultado) || Convert.ToInt32(misTextBoxs[i].Text) < 0)
+                {
+                    //si es cierto, mi bandera es false, entonces va a interpretar que lo campos no son validos
+                    bandera = false;
+                }
+            }
+            return bandera;
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Evento
+    {
+        public double estadioM2;
+        public int capacidadTribuna;
+        public double escenarioPorcentaje;
+        public double ticketPrecio;
+        public double ticketPrecioVIP;
+        public int calcularEntradas()
+        {
+            //calculo la cantidad de entradas y retorno el valor para mostrarlo en la propiedad .text asi lo vea el usuario
+            double tribunaM2 = estadioM2 * 0.2;
+            double escenarioM2 = (escenarioPorcentaje * estadioM2) / 100;
+            double espacioM2 = estadioM2 - tribunaM2 - escenarioM2;
+
+            int capacidad = Convert.ToInt32(espacioM2 * 4);
+            int capacidadTotal = capacidadTribuna + capacidad;
+            return capacidadTotal;
+        }
+        public double[] calcularIngresos(int cantidadEntradas)
+        {
+            int entradasVIP = Convert.ToInt32(cantidadEntradas * 0.3);
+            int entradasNoVIP = cantidadEntradas - entradasVIP;
+            double gananciasVIP = ticketPrecioVIP * entradasVIP;
+            double gananciasNoVIP = ticketPrecio * entradasNoVIP;
+            double gananciasTotales = gananciasVIP + gananciasNoVIP;
+            double[] ganancias = new double[3];
+            ganancias[0] = gananciasNoVIP;
+            ganancias[1] = gananciasVIP;
+            ganancias[2] = gananciasTotales;
+            return ganancias;
+        }
+        
+    }
+}
